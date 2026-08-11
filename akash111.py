ddec036a35ee72f2e70fc9f11b2d49940aa12ce3
Akash Kumar Biswas <akashkumarbiswassss@gmail.com>
2026-08-12 00:19:56 +0600

    Add files via upload

---
+++ akash111.py
@@ -0,0 +1,12 @@
+name=input("Enter your name:")
+age=input("Enter your age:")
+cgpa=input("Enter your cgpa:")
+address=input("Enter your address:")
+
+
+print("Student Information")
+print("........")
+print("name:"+name)
+print("age:"+age)
+print("cgpa:"+cgpa)
+print("address:"+address)
